25713aa16f82bddd8df22bef4a0d0a50fbcf0451
Valentina <vraurobayo@frba.utn.edu.ar>
2022-06-25 17:42:49 -0300

    subiendo resolucion

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,120 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+-- Auto
+data Auto = Auto {
+    color :: String,
+    velocidad:: Number,
+    distanciaRecorrida :: Number
+} deriving Show
+
+instance Eq Auto where {
+    (==) unAuto otroAuto = color unAuto == color otroAuto
+}
+
+-- Carrera
+type Carrera = [Auto]
+type PowerUp = (Auto -> Carrera -> Carrera)
+type Color = String
+type Posicion = (Number,Color)
+type Evento = (Carrera -> Carrera)
+
+
+-- estanCerca :: Auto -> Auto -> Bool
+-- estanCerca unAuto otroAuto =
+--     (unAuto /= otroAuto) && abs (distanciaRecorrida unAuto - distanciaRecorrida otroAuto) < 10
+
+estanCerca :: Auto -> Auto -> Bool
+estanCerca unAuto otroAuto = 
+    ((unAuto /= otroAuto &&) . (<10) . abs . (distanciaRecorrida unAuto -) . distanciaRecorrida) otroAuto
+
+vaTranquilo :: Auto -> Carrera -> Bool
+vaTranquilo auto carrera = 
+    (noTengaNingunAutoCerca auto carrera &&) . leVaGanandoATodos auto $ carrera
+
+noTengaNingunAutoCerca :: Auto -> Carrera -> Bool
+noTengaNingunAutoCerca auto = not . any (estanCerca auto)
+
+leVaGanandoATodos :: Auto -> Carrera -> Bool
+leVaGanandoATodos auto = not . any (`leVaGanando` auto)
+
+leVaGanandoATodos' :: Auto -> Carrera -> Bool
+leVaGanandoATodos' auto = all (leVaGanando auto)
+
+leVaGanando :: Auto -> Auto -> Bool
+leVaGanando unAuto otroAuto = 
+    ((unAuto /= otroAuto &&) . (< distanciaRecorrida unAuto) . distanciaRecorrida) otroAuto
+
+autoEnCarrera :: Auto -> Carrera -> Auto 
+autoEnCarrera auto = head . filter (== auto)
+
+puestoDeUnAuto :: Auto -> Carrera -> Number
+puestoDeUnAuto auto = (+1) . length . filter (`leVaGanando` auto)
+
+correrDurante :: Number -> Auto -> Auto
+correrDurante tiempo auto = auto{
+    distanciaRecorrida = distanciaRecorrida auto + tiempo * velocidad auto
+}
+
+
+-- bajarVelocidadEn :: Number -> Auto -> Auto 
+-- bajarVelocidadEn cuanto auto = auto{
+--     velocidad = modificador (velocidad auto - cuanto)
+-- }
+
+bajarVelocidadEn :: Number -> Auto -> Auto 
+bajarVelocidadEn = alterarVelocidad . flip (-)
+
+
+modificador :: Number -> Number
+modificador = max 0
+
+alterarVelocidad :: (Number -> Number) -> Auto ->Auto
+alterarVelocidad funcion auto = 
+    cambiarVelocidad (funcion . velocidad $ auto) auto
+
+-- duplicarVelocidad :: Auto -> Auto
+-- duplicarVelocidad auto = auto {
+--     velocidad = velocidad auto * 2
+-- }
+
+duplicarVelocidad :: Auto -> Auto
+duplicarVelocidad = alterarVelocidad (*2)
+
+cambiarVelocidad :: Number -> Auto -> Auto
+cambiarVelocidad velocidad auto = auto {
+    velocidad = modificador velocidad 
+}
+
+
+afectarALosQueCumplen :: (a -> Bool) -> (a -> a) -> [a] -> [a]
+afectarALosQueCumplen criterio efecto lista
+  = (map efecto . filter criterio) lista ++ filter (not.criterio) lista
+
+terremoto :: PowerUp
+terremoto auto  = afectarALosQueCumplen (estanCerca auto) (bajarVelocidadEn 50)
+
+miguelitos :: Number -> PowerUp
+miguelitos cuanto auto = afectarALosQueCumplen (`leVaGanando` auto) (bajarVelocidadEn cuanto)
+
+jetPack :: Number -> PowerUp
+jetPack tiempo auto = 
+    afectarALosQueCumplen (==auto) 
+    (cambiarVelocidad (velocidad auto) . correrDurante tiempo . duplicarVelocidad)
+
+obtenerPosiciones :: Carrera -> [Posicion]
+obtenerPosiciones carrera = 
+    map (\auto -> (puestoDeUnAuto auto carrera, color auto)) carrera
+
+simularCarrera :: Carrera -> [Evento] -> [Posicion]
+simularCarrera carrera = obtenerPosiciones . foldl (flip ($)) carrera 
+
+correnTodos :: Number -> Evento
+correnTodos = map . correrDurante 
+
+usaPowerUp :: PowerUp -> Color -> Evento
+usaPowerUp powerUp colorAuto carrera = 
+    powerUp (buscarPorColor colorAuto carrera) carrera
+
+buscarPorColor :: Color -> Carrera -> Auto
+buscarPorColor colorAuto = head . filter ((== colorAuto) . color)
--- src/Spec.hs
+++ src/Spec.hs
@@ -3,9 +3,92 @@ import PdePreludat
 import Library
 import Test.Hspec
 
+-- rojo
+rojo = Auto "rojo" 120 0
+-- azul
+azul = Auto "azul" 120 0
+-- verde
+verde = Auto "verde" 120 0
+-- blanco
+blanco = Auto "blanco" 120 0
+--negro
+negro = Auto "negro" 120 0
+
+-- carreraTranquilaParaElRojo
+carreraTranquilaParaElRojo = [rojo {distanciaRecorrida = 100}, azul, blanco]
+-- carreraNoTranquilaParaElRojo
+carreraNoTranquilaParaElRojo = [rojo {distanciaRecorrida = 100}, azul{distanciaRecorrida = 95}, blanco]
+-- carreraInicial
+carreraInicial = [rojo, azul, verde, blanco, negro]
+
+-- carreraAvanzada
+carreraAvanzada = [rojo, azul{distanciaRecorrida = 95}, blanco{distanciaRecorrida = 30}]
+
+-- eventos
+eventos = [correnTodos 30,
+           usaPowerUp (jetPack 3) "azul",
+           usaPowerUp terremoto "blanco",
+           correnTodos 30,
+           usaPowerUp (miguelitos 20) "blanco",
+           usaPowerUp (jetPack 6) "negro",
+           correnTodos 10]
+
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+ describe "Test de estan cerca" $ do
+  it "se cumple si son distintos y su distancia es menor a 10" $ do
+    rojo `shouldSatisfy` estanCerca azul {distanciaRecorrida = 5}
+  it "no se cumple si son iguales" $ do
+    rojo `shouldNotSatisfy` estanCerca rojo {distanciaRecorrida = 5}
+  it "no se cumple si son iguales" $ do
+    rojo `shouldNotSatisfy` estanCerca azul {distanciaRecorrida = 100}
+  it "no se cumple si la distancia es 10" $ do
+    rojo `shouldNotSatisfy` estanCerca rojo {distanciaRecorrida = 10}
+
+ describe "Test de va tranquilo" $ do
+  it "se cumple si no tiene ningún auto cerca y les va ganando a todos en una carrera" $ do
+    carreraTranquilaParaElRojo `shouldSatisfy` vaTranquilo (autoEnCarrera rojo carreraTranquilaParaElRojo)
+  it "no se cumple si tiene algun auto cerca" $ do
+    carreraNoTranquilaParaElRojo `shouldNotSatisfy` vaTranquilo (autoEnCarrera rojo carreraNoTranquilaParaElRojo)
+  it "no se cumple si no va ganando" $ do
+    carreraAvanzada `shouldNotSatisfy` vaTranquilo (autoEnCarrera rojo carreraAvanzada)
+
+ describe "Test de puesto de un auto" $ do
+  it "esta en primer puesto si le va ganando a todos" $ do
+    puestoDeUnAuto (autoEnCarrera rojo carreraTranquilaParaElRojo) carreraTranquilaParaElRojo `shouldBe` 1
+  it "esta en ultimp puesto si no le gana a nadie" $ do
+    puestoDeUnAuto (autoEnCarrera rojo carreraAvanzada) carreraAvanzada `shouldBe` 3
+
+ describe "Test de correr" $ do
+  it "si un auto corre por un tiempo, su distancia recorrida aumenta" $ do
+    distanciaRecorrida (correrDurante 1 rojo) `shouldBe` 120
+  it "si un auto corre 0 segundos, su distancia recorrida no cambia" $ do
+    distanciaRecorrida (correrDurante 0 rojo) `shouldBe` 0
+
+ describe "Test de modificar velocidad" $ do
+  it "su velocidad no dismuye mas que 0" $ do
+    velocidad (bajarVelocidadEn 1000 rojo) `shouldBe` 0
+  it "su velocidad dismuye en el numero que se indica" $ do
+    velocidad (bajarVelocidadEn 100 rojo) `shouldBe` 20
+
+ describe "Test Power Ups" $ do
+  it "test de terremoto" $ do
+    velocidad (autoEnCarrera azul (terremoto (autoEnCarrera rojo carreraNoTranquilaParaElRojo) carreraNoTranquilaParaElRojo)) `shouldBe` 70
+  it "test de miguelitos" $ do
+    velocidad (autoEnCarrera azul (miguelitos 100 (autoEnCarrera rojo carreraAvanzada) carreraAvanzada)) `shouldBe` 20
+  it "test de jetPack: la velocidad inicial es igual a la final" $ do
+    velocidad (autoEnCarrera azul (jetPack 100 azul carreraInicial)) `shouldBe` 120
+  it "test de jetPack: la distancia recorrida aumenta" $ do
+    distanciaRecorrida (autoEnCarrera azul (jetPack 2 azul carreraInicial)) `shouldBe` 480
+
+
+ describe "Test 4" $ do
+  it "test" $ do
+    1 `shouldBe` 1
+ describe "Test 4" $ do
+  it "test" $ do
+    1 `shouldBe` 1
+ describe "Test 4" $ do
+  it "test" $ do
+    1 `shouldBe` 1
 
